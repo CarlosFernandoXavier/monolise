e5024caf6414b9411fde0df794f893dee4fa20ed
Carlos Fernando <carlosf.s.x@hotmail.com>
2022-09-05 22:45:21 -0300

    Implementação da leitura de arquivos pela pasta zipada

--- src/main/java/MonoliseTest.java
+++ src/main/java/MonoliseTest.java
@@ -7,32 +7,81 @@ import response.ClassResponse;
 import response.MethodResponse;
 import response.Microservice;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
+import java.io.*;
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 public class MonoliseTest {
+
+    private final static String DIRETORIO_LEITURA = "src/main/resources/arquivos.zip";
+    private final static Double LIMITE_DECOMPOSICAO = 10.0;
 
     public static void main(String[] args) {
         executarMonolise();
     }
 
     public static void executarMonolise() {
-
-        //Trace de execução
-        List<String> arquivos = List.of("src/main/resources/gerar_relatorio_por_filial.txt",
-                "src/main/resources/buscar_itens.txt",
-                "src/main/resources/buscar_item_pelo_id.txt",
-                "src/main/resources/buscar_filiais.txt");
-
+        List<String> arquivos = buscarNomeArquivos(DIRETORIO_LEITURA);
         Map<String, List<Class>> functionalities = convertFunctionalityFiles(arquivos);
         Config config = convertApplicationConfigFile();
         setApplicationLayer(functionalities, config);
         Map<String, List<Column>> similarityTable = createSimilarityTable(functionalities);
         List<Microservice> microsservicos = groupFunctionalitiesBySimilatiry(similarityTable, config, functionalities);
         printMicrosservices(microsservicos);
+    }
+
+    private static List<String> buscarNomeArquivos(String diretorioLeitura) {
+        String diretorioDestino = "src/main/resources/output";
+        List<String> nomeArquivos = new ArrayList<>();
+        descompactar(diretorioLeitura, diretorioDestino, nomeArquivos);
+        return getPathArquivos(diretorioDestino, nomeArquivos);
+    }
+
+
+    private static List<String> getPathArquivos(String diretorioDestino, List<String> nomeArquivos) {
+        List<String> pathArquivos = new ArrayList<>();
+        nomeArquivos.forEach(nome -> pathArquivos.add(diretorioDestino + "/" + nome));
+        return pathArquivos;
+    }
+
+    private static void descompactar(String zipFilePath, String destDir, List<String> nomeArquivos) {
+        File dir = new File(destDir);
+        // create output directory if it doesn't exist
+        if (!dir.exists()) dir.mkdirs();
+        FileInputStream fis;
+        //buffer for read and write data to file
+        byte[] buffer = new byte[1024];
+        try {
+            fis = new FileInputStream(zipFilePath);
+            ZipInputStream zis = new ZipInputStream(fis);
+            ZipEntry ze = zis.getNextEntry();
+            while (ze != null) {
+                String fileName = ze.getName();
+                nomeArquivos.add(fileName);
+                File newFile = new File(destDir + File.separator + fileName);
+                System.out.println("Unzipping to " + newFile.getAbsolutePath());
+                //create directories for sub directories in zip
+                new File(newFile.getParent()).mkdirs();
+                FileOutputStream fos = new FileOutputStream(newFile);
+                int len;
+                while ((len = zis.read(buffer)) > 0) {
+                    fos.write(buffer, 0, len);
+                }
+                fos.close();
+                //close this ZipEntry
+                zis.closeEntry();
+                ze = zis.getNextEntry();
+            }
+            //close last ZipEntry
+            zis.closeEntry();
+            zis.close();
+            fis.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
     private static Map<String, List<Class>> convertFunctionalityFiles(List<String> functionalityFiles) {
@@ -56,6 +105,7 @@ public class MonoliseTest {
 
                     //TODO corrigir o nome dessa variável abaixo
                     List<String> t = new ArrayList<>();
+
                     t.add(linha[1].substring(linha[1].lastIndexOf(": ") + 2));
                     classe.setMethodName(t);
 
@@ -104,7 +154,7 @@ public class MonoliseTest {
         config.setServiceWeight(1.0);
         config.setDtoWeight(0.3);
         config.setControllerWeight(0.5);
-        config.setDecompositionThreshold(80.0);
+        config.setDecompositionThreshold(LIMITE_DECOMPOSICAO);
         return config;
     }
 
